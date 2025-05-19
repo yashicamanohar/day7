306e6b7faab7ad726c137e57e86d63480a798b82
yashicamanohar <yashicamanoharofficial@gmail.com>
2025-05-19 13:20:51 +0530

    Add files via upload

---
+++ bubbles.c
@@ -0,0 +1,31 @@
+I am Sreekanth V . I have completed my bachelor's degree in adhiyaman college of engineering in the streams of Electronics and communication engineering with my cgpa is 8.
+
+In my academics I learned the concepts of Java, Html , Css and Java script. After completing my academics,  I learned C#, asp.net framework, ado.net and sql server.
+
+Based on my learning I have done my project as Cab services.This is the beginning level of cab booking Management system using .net  framework .
+
+In my academics I attended the workshop as IOT for home appliances. In this workshop I learned about IOT and importance of IOT.
+
+As a fresher, I believe my adaptability,  eagerness, strong work ethics made me a valuable addition to any team.
+
+Thank you for giving me the opportunity to introduce myself.
+
+
+
+In my project I create the Cab services web application which is the beginning level of cab booking Management system.
+
+I use the aspx.net for the front end development and using css for styling, ado.net for connection between web server and databases, sql server for database.
+
+My project contains the home page,  admin login, user login and about us.
+
+Admin login with username and password which is already set in admin table.
+
+In user login, user sign up their login details which is stored in userdetails table and password must be encrypted.
+
+In home page, user see the car models , seat availability and booking forms.
+
+In about us page contains details of the Cab services.
+
+After sign up their details, user book the cab through booking form. After confirm the booking, it will store in booking_details, and admin see the details of booking from the admin page. 
+
+To prevent the sql injection attack, I used the stored procedure for inserting and updating the tables.
