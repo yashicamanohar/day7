1eae747e3361d613d20088cd626b8ee2c6df106d
yashicamanohar <yashicamanoharofficial@gmail.com>
2025-05-19 15:49:35 +0530

    Add files via upload

---
+++ test question.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int c,m;
+    scanf("%d%d",&c,&m);
+    for(int i=0;i<c;i++){
+    int sum=0,fule;
+    for(int j=0;j<m;j++){
+    sum+=fule;
+    scanf("%d",&fule);
+    
+    }
+    printf("%d\n",sum);
+    }
+}
